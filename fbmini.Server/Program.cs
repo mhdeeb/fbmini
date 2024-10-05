1eb785e2dab831881cd444a8f8ab68dbd169ff2e
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2024-10-05 16:11:40 +0300

    Test

--- fbmini.Server/Program.cs
+++ fbmini.Server/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using fbmini.Server.Data;
 using fbmini.Server.Controllers;
+using Azure.Identity;
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 builder.Services.AddDbContext<fbminiServerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("fbminiServerContext") ?? throw new InvalidOperationException("Connection string 'fbminiServerContext' not found.")));
 
