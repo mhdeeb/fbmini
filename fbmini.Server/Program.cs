3e4abb107c1dd07bb59e507c1bc78a2814ec780a
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2024-10-05 18:44:15 +0300

    API

--- fbmini.Server/Program.cs
+++ fbmini.Server/Program.cs
@@ -10,24 +10,11 @@ builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredent
 builder.Services.AddDbContext<fbminiServerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("fbminiServerContext") ?? throw new InvalidOperationException("Connection string 'fbminiServerContext' not found.")));
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//app.UseDefaultFiles();
 app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
 
 app.UseHttpsRedirection();
 
