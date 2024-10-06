6acd91656068f34a9dbed2b32af8d7eb1b8bcf4e
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2024-10-06 19:08:20 +0300

    F

--- fbmini.Server/Program.cs
+++ fbmini.Server/Program.cs
@@ -2,7 +2,7 @@
 using fbmini.Server.Data;
 using fbmini.Server.Controllers;
 using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
--- fbmini.client/src/proxy.conf.js
+++ fbmini.client/src/proxy.conf.js
@@ -1,7 +1,10 @@
+const { Console } = require('console');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7223';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7223';
+
+console.log(target);
 
 const PROXY_CONFIG = [
   {
