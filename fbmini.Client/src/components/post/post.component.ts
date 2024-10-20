1519cb388f5d771dc91fdc712e8077869e8d045a
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2024-10-20 21:15:14 +0300

    Test

--- fbmini.Client/src/components/post/post.component.ts
+++ fbmini.Client/src/components/post/post.component.ts
@@ -3,13 +3,20 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrl: './post.component.css',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    CommonModule,
+  ],
 })
 export class PostComponent {
   @Input() post: any;
